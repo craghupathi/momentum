2bc8c8c1b705e067e02b7aa2b55ed5282650fed6
Riya Ranjan <riya.ranjan.00@gmail.com>
2020-06-25 14:23:06 -0700

    committing new files

--- momentum/ios/Flutter/flutter_export_environment.sh
+++ momentum/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/riyaranjan/AppDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/riyaranjan/AppDev/momentum"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/riyaranjan/AppDev/CongressionalApp/momentum/momentum"
+export "FLUTTER_TARGET=/Users/riyaranjan/AppDev/CongressionalApp/momentum/momentum/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/riyaranjan/AppDev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
