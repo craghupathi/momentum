6c1f9ccc159233f94d616cd645843aed518d0f69
Riya Ranjan <riya.ranjan.00@gmail.com>
2020-07-03 16:08:23 -0700

    Removed XD files, added first steps to home page

--- momentum/lib/main.dart
+++ momentum/lib/main.dart
@@ -1,8 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:adobe_xd/adobe_xd.dart';
-import 'package:momentum/iPhoneXXS11Pro1.dart';
 
-void main() => runApp(MaterialApp(
-      home: iPhoneXXS11Pro1(),
-    ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Momentum',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xff8f5170),
+          title: Text('momentum',
+              style: TextStyle(
+                fontFamily: 'Gill Sans',
+                fontSize: 30,
+                fontWeight: FontWeight.w300,
+              )),
+        ),
+        body: Center(child: WelcomeButton()),
+      ),
+    );
+  }
+}
+
+class WelcomeButton extends StatefulWidget {
+  @override
+  _WelcomeButtonState createState() => _WelcomeButtonState();
+}
+
+class _WelcomeButtonState extends State<WelcomeButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 1000.0,
+      height: 1000.0,
+      child: ButtonBar(
+        alignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+              color: Color(0xff8f5170),
+              onPressed: () {},
+              child: Text('Login',
+                  style: TextStyle(
+                    fontFamily: 'Gill Sans',
+                    fontSize: 20,
+                    color: Color(0xffffffff),
+                    fontWeight: FontWeight.w300,
+                  )),
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              highlightColor: Color(0xffDAB6C4),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Color(0xffffffff)))),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Sign Up',
+                style: TextStyle(
+                  fontFamily: 'Gill Sans',
+                  fontSize: 19.5,
+                  color: Color(0xffffffff),
+                  fontWeight: FontWeight.w300,
+                )),
+            color: Color(0xff8f5170),
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            highlightColor: Color(0xffDAB6C4),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Color(0xffffffff))),
+          )
+        ],
+      ),
+    );
+  }
+}
